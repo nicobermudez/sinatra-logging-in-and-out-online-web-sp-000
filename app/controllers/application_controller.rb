68ee843389acb8bc567c2675c957935725ebbad3
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-01 16:10:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(params[:username])
     if @user && @user.password == params[:password]
-      sessoin[:user_id] = @user.id
+      session[:user_id] = @user.id
   end
 
   get '/account' do
