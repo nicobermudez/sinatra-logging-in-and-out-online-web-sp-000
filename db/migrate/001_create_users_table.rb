4a0a212de7e7ce89a08e415c91ece9a7e7681ccc
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-01 15:57:15 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_users_table.rb
+++ db/migrate/001_create_users_table.rb
@@ -1,8 +1,8 @@
 class CreateUserTable < ActiveRecord::Migration
   def change
     create_table users do |t|
-      t.string :name 
-      t.string :password 
+      t.string :name
+      t.string :password
       t.integer :balance
     end
   end
